2e41598cdab6c85e067cd7627f1347dafe07daf8
N-A-T-H-A-N-2017 <natkwong06fj@yahoo.com>
2017-05-08 21:11:25 +1200

    Add files via upload

---
+++ CITY_CAMPUS.java
@@ -0,0 +1,13 @@
+package com.example.hwt3314.maps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CITY_CAMPUS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city__campus);
+    }
+}
