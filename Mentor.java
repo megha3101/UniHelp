4eb33aea6a4f86de2200e5785ad274cde123d581
alexanderglenn <alexanderjamesglenn@gmail.com>
2017-06-08 16:37:25 +1200

    Add files via upload
    Rating System code version 1 (Not working yet)

---
+++ Mentor.java
@@ -0,0 +1,58 @@
+package com.example.alexg.uni_helper_aoo;
+
+/**
+ * Created by alexg on 2/06/2017.
+ */
+
+public class Mentor {
+
+    private string name;
+    private int mentor_rating = 0;
+    private int num_of_ratings = 0;
+    private int absolute_ratings = 0;   // This is the total number of stars received. This will
+                                        // be used for calculation later
+
+    public Mentor(string name){
+        setName(name);
+    }
+
+    // Getter and setter methods below
+    public void setName(string name){
+        this.name = name;
+    }
+
+    public int getAbsolute_ratings(){
+        return absolute_ratings;
+    }
+
+    public void setAbsolute_ratings(int star_input){
+        absolute_ratings += star_input;
+    }
+
+    public int getNum_of_ratings(){
+        return num_of_ratings;
+    }
+
+    // Increments number of ratings made when a new rating is entered.
+    public void setNum_of_ratings(){
+        num_of_ratings++;
+    }
+
+    public void setMentor_rating(int rating){
+
+        int m_rating = getCurrentMentor_rating();
+        int abs_rating = getAboslute_ratings();
+
+        // update the number of ratings made
+        setNum_of_ratings();
+
+        int num_ratings = getNum_of_ratings();
+
+        // Mentor rating = sum of ratings / number of ratings
+        mentor_rating = abs_rating / num_ratings;
+    }
+
+    public int getCurrentMentor_rating() {
+        return mentor_rating;
+    }
+}
---
+++ RatingsActivity.java
@@ -0,0 +1,81 @@
+package com.example.alexg.uni_helper_aoo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class RatingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ratings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public static class Ratings_List_activity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_ratings__list_activity);
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
+            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
+        }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_ratings__list_activity, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
+
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public static class RatingsAddActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_ratings_add);
+        }
+    }
+}
